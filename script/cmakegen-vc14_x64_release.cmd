923c33bb96c4646743e11b25f2ee124993d2825e
Oleg Kolomiytsev <define.kolomiytsev@gmail.com>
2017-04-22 00:09:17 +0300

    Fixed build scripts.

---
+++ script/cmakegen-vc14_x64_release.cmd
@@ -0,0 +1,40 @@
+@echo off
+
+:: Check build directory.
+set BUILD_DIR=..\build\vc14-x64
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+pushd %BUILD_DIR%
+
+:: Run Conan.
+echo [*] Conan is running...
+conan ^
+	install ^
+	../.. ^
+	-s arch=x86_64 ^
+	-s build_type=Release ^
+	-s compiler="Visual Studio" ^
+	-s compiler.runtime=MT ^
+	-s compiler.version=14 ^
+	-s os=Windows ^
+	--build missing
+if errorlevel 1 goto :error
+
+:: Run CMake.
+echo [*] CMake is running...
+cmake ^
+	-G"Visual Studio 14 2015 Win64" ^
+	-T "v140_xp" ^
+	"-DBUILD_WITH_STATIC_CRT:BOOL=ON" ^
+	../..
+if errorlevel 1 goto :error
+
+:: Exit
+echo [*] Project generation succeeded!
+popd
+goto :eof
+
+:: Error handling
+:error
+echo [!] ERROR: Failed to generate project - see above and correct.
+popd
+exit /b 1
--- script/cmakegen-vc14_x86_release.cmd
+++ script/cmakegen-vc14_x86_release.cmd
@@ -6,20 +6,21 @@ if not exist %BUILD_DIR% mkdir %BUILD_DIR%
 pushd %BUILD_DIR%
 
 :: Run Conan
-rem echo -- Conan is running!
-rem conan ^
-rem	install ^
-rem	../.. ^
-rem	-s build_type=Debug ^
-rem	-s compiler="Visual Studio" ^
-rem	-s compiler.version=14 ^
-rem	-s compiler.runtime=MT ^
-rem	-s arch=x86 ^
-rem	--build missing
-rem if errorlevel 1 goto :error
+echo [*] Conan is running...
+conan ^
+	install ^
+	../.. ^
+	-s arch=x86 ^
+	-s build_type=Release ^
+	-s compiler="Visual Studio" ^
+	-s compiler.runtime=MT ^
+	-s compiler.version=14 ^
+	-s os=Windows ^
+	--build missing
+if errorlevel 1 goto :error
 
 :: Run CMake.
-echo -- Cmake is running!
+echo [*] CMake is running...
 cmake ^
 	-G"Visual Studio 14 2015" ^
 	-T "v140_xp" ^
